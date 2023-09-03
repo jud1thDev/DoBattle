bcf5ead98abff2e7e0e70ba99fdf26db87a3a31f
Bokjihee <bokjihee@naver.com>
2023-09-04 06:30:14 +0900

    싹밀어버리기

--- src/main/resources/static/js/battle.js
+++ src/main/resources/static/js/battle.js
@@ -1,14 +1,23 @@
 let todoNum = document.querySelectorAll('#battle-bottom > form').length;    //몇번째 todo인지 class명 저장용
 let txtFieldNum = 0; // 줄줄이 투두 수정용
-let clicked = 0; // 단순 투두 몇 개 끝냄?
-let percent = 0;
 
-const form = document.querySelectorAll('form');
-const actionUrl = form[0].getAttribute('action');
+//퍼센트계산용 변수들
+let clicked = 0;    //단순 투두 몇개 끝냄?
+let checkValue = document.querySelectorAll('.fire-button');
+for(let i=0; i<checkValue.length; i++){
+    if(checkValue[i].value === 'done')
+        clicked++;  //value값이 done인만큼 개수 저장
+}
+let percent = (clicked/todoNum)*100;
 
+
+//길이 변경용 변수들
+let me = document.querySelector('#me > .progress > .first');
+let other = document.querySelector('#other > .progress > .first');
 window.onload = function() {
     changeFireColor();
-    console.log(actionUrl);
+    percentCalc(me);
+    percentCalc(other);
 }
 
 function changeFireColor(){
@@ -23,63 +32,6 @@ function changeFireColor(){
 
     }
 }
-
-//function todoClick() {
-//  while (txtFieldNum === 0) {
-//    let todo;   //입력한 문구 저장용
-//    let battleBottom = document.getElementById("battle-bottom");
-//
-//    let newDiv = document.createElement("div"); // 전체 감쌀 div
-//    let newText = document.createElement("input");
-//    newText.type = "text";
-//    newText.className = "input-text";
-//    newText.placeholder = "입력";
-//
-//    battleBottom.appendChild(newDiv);
-//    newDiv.appendChild(newText);
-//
-//    txtFieldNum++; // 줄줄이 방지
-//
-//    newText.addEventListener("keyup", function (event) {
-//      if (event.key === "Enter") {
-//        todo = newText.value;
-////        if (!todo) {
-////            alert("오늘 할 일을 입력하세요.");
-////            return;
-////        }
-//        battleBottom.removeChild(newDiv);   //위에 input으로 만든거 전체 삭제
-//
-//        let newDiv2 = document.createElement("form");    //밖에 감싸줄 form 태그
-//        //newDiv2.setAttribute("action", "/battle");    //html안의 js 파일에서 제어해줄것
-//        newDiv2.setAttribute("method", "post");
-//
-//        let todoList = document.createElement("div");   //todo 저장
-//        todoList.className = "todo-list";
-//        todoList.innerText = todo;
-//
-//        let fireImg = document.createElement("input"); // 불 input 체크박스
-//        fireImg.type = "submit";
-//        fireImg.value = "notDone";
-//        fireImg.className = "fire-button";
-//        fireImg.name = "value";
-//
-//        let hiddenInput1 = document.createElement("input");
-//        hiddenInput1.type = "hidden"; // 숨겨진 필드로 데이터 전달
-//        hiddenInput1.name = "todoDataValue"; // 백엔드에서 사용
-//        hiddenInput1.value = todo;
-//        hiddenInput1.className = "todoDataValue";
-//
-//        battleBottom.appendChild(newDiv2);
-//        newDiv2.appendChild(todoList);
-//        newDiv2.appendChild(fireImg);
-//        newDiv2.appendChild(hiddenInput1);
-//
-//        todoNum++;
-//        txtFieldNum--;
-//      }
-//    });
-//  }
-//}
 
 function fireOrange(num) {
   let allDiv = document.getElementById(num);
@@ -99,6 +51,7 @@ function fireOrange(num) {
     todoList.style.borderBottom = "2px solid white";
   }
 }
+
 function changeValueAndSubmit(self) {
     if (self.value === 'notDone') {
         self.value = 'done';
@@ -116,25 +69,25 @@ function changeValueAndSubmit(self) {
     }
 }
 
-function saveTodoData() {
-  var battleCode = document.getElementById("battleCode").value;
-  var todoDataValue = document.getElementById("todoDataValue").value;
-  var value = document.getElementById("value").value;
-
-  $.ajax({
-    type: "POST",
-    url: "/battle/" + battleCode + "/saveTodoData",
-    data: {
-      battleCode: battleCode,
-      todoDataValue: todoDataValue,
-      value: value,
-    },
-    success: function (response) {
-      console.log(response);
-      // 여기에 서버 응답을 처리하는 로직을 추가할 수 있습니다.
-    },
-    error: function () {
-      alert("데이터 저장 중 오류가 발생했습니다.");
-    },
-  });
-}
+//function saveTodoData() {
+//  var battleCode = document.getElementById("battleCode").value;
+//  var todoDataValue = document.getElementById("todoDataValue").value;
+//  var value = document.getElementById("value").value;
+//
+//  $.ajax({
+//    type: "POST",
+//    url: "/battle/" + battleCode + "/saveTodoData",
+//    data: {
+//      battleCode: battleCode,
+//      todoDataValue: todoDataValue,
+//      value: value,
+//    },
+//    success: function (response) {
+//      console.log(response);
+//      // 여기에 서버 응답을 처리하는 로직을 추가할 수 있습니다.
+//    },
+//    error: function () {
+//      alert("데이터 저장 중 오류가 발생했습니다.");
+//    },
+//  });
+//}
