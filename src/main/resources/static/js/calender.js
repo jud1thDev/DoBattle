ba51e60d68f700bb818fcd451dbe87134c2592df
Bokjihee <bokjihee@naver.com>
2024-03-01 03:12:14 +0900

    [fe] feat: 백에서 정보 받아서 승리자따라 달력 불 색 변화

--- src/main/resources/static/js/calender.js
+++ src/main/resources/static/js/calender.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    buildCalendar();
+    CalendarCallBack();
     document.getElementById("click-date").innerText = (nowMonth.getMonth() + 1) + '/' + today.getDate();
     whoWin();
 }
@@ -7,8 +7,35 @@ window.onload = function () {
 let nowMonth = new Date();  // 현재 달을 페이지를 로드한 날의 달로 초기화
 let today = new Date();     // 오늘 날 저장용
 let changeDay;  //밑에 결과창용
+let serverData; //서버에서 넘어온 결과 저장용
+
+function CalendarCallBack(){
+    //api 호출
+    let querySend = '/calender/fire/' + battleCode;
+
+    fetch(querySend,{
+        method: "GET"
+    }).then(response => {
+        //응답이 성공적으로 받아졌을 때 처리
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        serverData = data;
+        //캘린더 만들기 불러오기
+        buildCalendar();
+    })
+    .catch(error => {
+        // 오류 발생 시 처리
+        console.log('There was a problem with the fetch operation:', error);
+    });
+}
 
 function buildCalendar(){
+    /**달력만들기*/
     let firstDate = new Date(nowMonth.getFullYear(), nowMonth.getMonth(), 1);   //이번달 1일
     let lastDate = new Date(nowMonth.getFullYear(), nowMonth.getMonth()+1, 0);  //이번달 마지막일
 
@@ -32,6 +59,11 @@ function buildCalendar(){
     //1일 이후 달력
     let nowDay = new Date(firstDate.getFullYear(), firstDate.getMonth(), firstDate.getDate(), 0, 0); //1일부터 시작 (시간 0시 0초로 맞춤)
     while (nowDay <= lastDate) {
+        //서버에서 불러오기 위한 LocalDate형 날짜
+        let serverDate = nowDay.getFullYear() + "-" + (nowDay.getMonth() + 1).toString().padStart(2, '0') + "-" + nowDay.getDate().toString().padStart(2, '0');
+        let startDate = new Date(start);
+        console.log(serverDate);
+
         if (nowDay.getDay() === 0) { // 일요일인 경우 새로운 행을 추가
             nowRow = Calendar_tbody.insertRow();
         }
@@ -40,9 +72,25 @@ function buildCalendar(){
         //불svg 넣기
         let newDivFire = document.createElement("div");
         let imgElement = document.createElement("img");
-        //오늘 이후는 투명도 70%
-        if(today >= nowDay){
-            imgElement.src = "/image/cal-fire-white.svg";
+        //오늘 이후는 투명도 70%, 오늘 이후&시작날짜 이전의 날짜는 클릭 불가
+        if(nowDay <= today && nowDay >= startDate){
+            if(serverData[serverDate] == "동점"){
+                imgElement.src = "/image/cal-fire-white.svg";
+                console.log("동점");
+            }
+            else if(serverData[serverDate] == "파트너"){
+                imgElement.src = "/image/cal-fire-grey.svg";
+                console.log("파트너 승");
+            }
+            else{
+                imgElement.src = "/image/cal-fire-orange.svg";
+                console.log("본인 승");
+            }
+
+            newDivFire.onclick = function() {
+                changeDay = newDivFire.id;
+                changeDate(changeDay);
+            };
         } else{
             imgElement.src = "/image/cal-fire-white70.svg";
         }
@@ -54,7 +102,7 @@ function buildCalendar(){
         newDivDate.innerHTML = nowDay.getDate();        // 추가한 열에 날짜 입력
         nowColumn.appendChild(newDivDate);
 
-        //오늘 날짜 주황색으로 표시
+        //오늘 날짜 글씨 주황색으로 표시
         if (nowDay.getFullYear() == today.getFullYear() && nowDay.getMonth() == today.getMonth() && nowDay.getDate() == today.getDate()){
             newDivDate.className = "today";
         }
@@ -62,19 +110,11 @@ function buildCalendar(){
         //밑에 날짜 뜨기 위한 id값 저장
         nowColumn.id = nowDay.getDate();
         newDivFire.id = nowDay.getDate();
-        //클릭시 밑에 날짜뜨는 (오늘 이전의 날만)
-        if(today >= nowDay){
-            newDivFire.onclick = function() {
-                changeDay = newDivFire.id;
-                changeDate(changeDay);
-            };
-        }
 
         // 다음 날짜로 이동
         nowDay.setDate(nowDay.getDate() + 1);
     }
 }
-
 
 function prevCalendar(){
     nowMonth.setMonth(nowMonth.getMonth()-1);
